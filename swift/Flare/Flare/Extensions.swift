90f2dc392ba33c58bd79ee6c38d6b258c8bc3ff3
Andrew Zamler-Carhart <azamlerc@cisco.com>
2016-02-22 14:26:24 +0100

    Added 3D support

--- swift/Flare/Flare/Extensions.swift
+++ swift/Flare/Flare/Extensions.swift
@@ -12,9 +12,38 @@ import CoreGraphics
 public typealias JSONDictionary = [String:AnyObject]
 public typealias JSONArray = [JSONDictionary]
 
+public struct Point3D {
+    var x: CGFloat
+    var y: CGFloat
+    var z: CGFloat
+}
+
+public struct Size3D {
+    var width: CGFloat
+    var height: CGFloat
+    var depth: CGFloat
+}
+
+public struct Cube3D {
+    var origin: Point3D
+    var size: Size3D
+}
+
 // compare JSONDictionary objects
 public func ==(lhs: JSONDictionary, rhs: JSONDictionary) -> Bool {
     return NSDictionary(dictionary: lhs).isEqualToDictionary(rhs)
+}
+
+public func ==(a: Point3D, b: Point3D) -> Bool {
+    return a.x == b.x && a.y == b.y && a.z == b.z
+}
+
+public func ==(a: Size3D, b: Size3D) -> Bool {
+    return a.width == b.width && a.height == b.height && a.depth == b.depth
+}
+
+public func ==(a: Cube3D, b: Cube3D) -> Bool {
+    return a.origin == b.origin && a.size == b.size
 }
 
 // a point translated by the given size
@@ -24,6 +53,13 @@ public func +(point: CGPoint, size: CGSize) -> CGPoint {
     return CGPoint(x:x, y:y)
 }
 
+public func +(point: Point3D, size: Size3D) -> Point3D {
+    let x = point.x + size.width
+    let y = point.y + size.height
+    let z = point.z + size.depth
+    return Point3D(x:x, y:y, z:z)
+}
+
 // a point negatively translated by the given size
 public func -(point: CGPoint, size: CGSize) -> CGPoint {
     let x = point.x - size.width
@@ -31,20 +67,25 @@ public func -(point: CGPoint, size: CGSize) -> CGPoint {
     return CGPoint(x:x , y:y)
 }
 
-// the distance between two points (width and height)
-/*
-func -(point1: CGPoint, point2: CGPoint) -> CGSize {
-    let width = point1.x - point2.x
-    let height = point1.y - point2.y
-    return CGSize(width:width, height:height)
+public func -(point: Point3D, size: Size3D) -> Point3D {
+    let x = point.x - size.width
+    let y = point.y - size.height
+    let z = point.z - size.depth
+    return Point3D(x:x, y:y, z:z)
 }
-*/
 
 // the distance between two points (diagonal)
 public func -(point1: CGPoint, point2: CGPoint) -> Double {
     let dx = point1.x - point2.x
     let dy = point1.y - point2.y
     return Double(sqrt(dx * dx + dy * dy))
+}
+
+public func -(point1: Point3D, point2: Point3D) -> Double {
+    let dx = point1.x - point2.x
+    let dy = point1.y - point2.y
+    let dz = point1.z - point2.z
+    return Double(sqrt(dx * dx + dy * dy + dz * dz))
 }
 
 // the difference between two sizes
@@ -54,11 +95,25 @@ public func -(size1: CGSize, size2: CGSize) -> CGSize {
     return CGSize(width:width, height:height)
 }
 
+public func -(size1: Size3D, size2: Size3D) -> Size3D {
+    let width = size1.width - size2.width
+    let height = size1.height - size2.height
+    let depth = size1.depth - size2.depth
+    return Size3D(width:width, height:height, depth:depth)
+}
+
 // a point scaled by the given ratio
 public func *(point: CGPoint, ratio: CGFloat) -> CGPoint {
     let x = point.x * ratio
     let y = point.y * ratio
     return CGPoint(x:x, y:y)
+}
+
+public func *(point: Point3D, ratio: CGFloat) -> Point3D {
+    let x = point.x * ratio
+    let y = point.y * ratio
+    let z = point.z * ratio
+    return Point3D(x:x, y:y, z:z)
 }
 
 // a size scaled by the given ratio
@@ -68,11 +123,24 @@ public func *(size: CGSize, ratio: CGFloat) -> CGSize {
     return CGSize(width:width, height:height)
 }
 
+public func *(size: Size3D, ratio: CGFloat) -> Size3D {
+    let width = size.width * ratio
+    let height = size.height * ratio
+    let depth = size.depth * ratio
+    return Size3D(width:width, height:height, depth:depth)
+}
+
 // a rect scaled by the given ratio
 public func *(rect: CGRect, ratio: CGFloat) -> CGRect {
     let origin = rect.origin * ratio
     let size = rect.size * ratio
     return CGRect(origin:origin, size:size)
+}
+
+public func *(rect: Cube3D, ratio: CGFloat) -> Cube3D {
+    let origin = rect.origin * ratio
+    let size = rect.size * ratio
+    return Cube3D(origin:origin, size:size)
 }
 
 // returns a random Int between 0 and limit - 1
@@ -90,6 +158,13 @@ public func randomSize(min: Int, max: Int) -> CGSize {
     let width = randomInt(min, max: max)
     let height = randomInt(min, max: max)
     return CGSize(width:width, height:height)
+}
+
+public func randomSize3D(min: Int, max: Int) -> Size3D {
+    let width = randomInt(min, max: max)
+    let height = randomInt(min, max: max)
+    let depth = randomInt(min, max: max)
+    return Size3D(width:CGFloat(width), height:CGFloat(height), depth:CGFloat(depth))
 }
 
 public extension Array {
@@ -144,6 +219,20 @@ public extension CGRect {
     }
 }
 
+public extension Cube3D {
+    
+    // returns the point at the center of the cube
+    func center() -> Point3D {
+        return Point3D(x: self.origin.x + self.size.width / 2.0,
+            y: self.origin.y + self.size.height / 2.0,
+            z: self.origin.z + self.size.depth / 2.0)
+    }
+    
+    public func toJSON() -> JSONDictionary {
+        return ["origin": self.origin.toJSON(), "size": self.size.toJSON()]
+    }
+}
+
 public extension CGSize {
     
     // returns the diagonal length
@@ -158,6 +247,21 @@ public extension CGSize {
     }
 }
 
+public extension Size3D {
+    
+    // returns the diagonal length
+    public func length() -> Double {
+        let x2 = Double(self.width * self.width)
+        let y2 = Double(self.height * self.height)
+        let z2 = Double(self.depth * self.depth)
+        return sqrt(x2 + y2 + z2)
+    }
+    
+    public func toJSON() -> JSONDictionary {
+        return ["width": self.width, "height": self.height, "depth": self.depth]
+    }
+}
+
 public extension CGPoint {
     
     // rounds the point's x and y values to the given precision
@@ -167,6 +271,20 @@ public extension CGPoint {
     
     public func toJSON() -> JSONDictionary {
         return ["x": self.x, "y": self.y]
+    }
+}
+
+public extension Point3D {
+    
+    // rounds the point's x and y values to the given precision
+    public func roundTo(precision: Double) -> Point3D {
+        return Point3D(x: CGFloat(self.x.roundTo(precision)),
+            y: CGFloat(self.y.roundTo(precision)),
+            z: CGFloat(self.z.roundTo(precision)))
+    }
+    
+    public func toJSON() -> JSONDictionary {
+        return ["x": self.x, "y": self.y, "z": self.z]
     }
 }
 
